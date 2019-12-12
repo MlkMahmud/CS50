3fc8c3ad141a51bbadf754c11ddb84b3e5756fd4
Malik Mahmud <almalikmahmud@gmail.com>
2019-12-12 12:54:49 +0100

    Add vigenere solution

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -16,7 +16,7 @@ int main (int argc, char *argv[]) {
   }
 
 
-  printf("plaintext: ");
+  puts("plaintext: ");
   fgets(plaintext, sizeof(plaintext), stdin);
 
   long textLength = strlen(plaintext);
